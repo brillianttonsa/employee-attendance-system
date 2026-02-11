8b9bf0352bcbfef540e7e69c7831f24c320ddcb1
brillianttonsa <abdullatifmnyamis@gmail.com>
2026-02-12 00:03:10 +0300

    Admin frontend and backend version 1 complete

---
+++ src/pages/Test.tsx
@@ -0,0 +1,23 @@
+import WorkersPage from "./dashboard/admin/WorkersPage"
+import DepartmentsPage from "./dashboard/admin/DepartmentsPage"
+import SettingsPage from "./dashboard/admin/SettingsPage"
+import ReportsPage from "./dashboard/admin/ReportsPage"
+import AttendancePage from "./dashboard/admin/AttendancePage"
+import SessionsPage from "./dashboard/admin/SessionsPage"
+import ScanPage from "./ScanPage"
+import AdminDashboard from "./dashboard/admin/AdminDashboard"
+
+export default function Test() {
+  return (
+    <div className="m-8">
+        <AdminDashboard/>
+        {/* <WorkersPage/>
+        <DepartmentsPage/> */}
+        {/* <SettingsPage/> */}
+        {/* <ReportsPage/> */}
+        {/* <AttendancePage/> */}
+        {/* <SessionsPage/> */}
+        {/* <ScanPage/> */}
+    </div>
+  )
+}
